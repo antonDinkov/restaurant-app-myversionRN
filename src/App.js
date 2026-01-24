960c019b1ba7eebc0f70b93ad31471cc570c9163
antonDinkov <166150125+antonDinkov@users.noreply.github.com>
2026-01-24 16:09:27 +0700

    icons added to the bottom tab

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import RootNavigator from './navigation/RootNavigator';
 
 export default function App() {
--- src/navigation/RootNavigator.jsx
+++ src/navigation/RootNavigator.jsx
@@ -2,14 +2,15 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
 import HomeNavigator from "../rootScreens/homeTab/HomeNavigator";
 import CartNavigator from "../rootScreens/cartTab/CartNavigator";
 import InfoNavigator from "../rootScreens/infoTab/InfoNavigator";
+import { Ionicons } from "@expo/vector-icons"
 
 export default function RootNavigator(props) {
     const Tabs = createBottomTabNavigator();
     return (
         <Tabs.Navigator>
-            <Tabs.Screen name="Home" component={HomeNavigator} options={{headerShown: false}} />
-            <Tabs.Screen name="Cart" component={CartNavigator} options={{headerShown: false}} />
-            <Tabs.Screen name="Info" component={InfoNavigator} options={{headerShown: false}} />
+            <Tabs.Screen name="Home" component={HomeNavigator} options={{headerShown: false, tabBarIcon: ({focused, color, size}) => <Ionicons name="home" size={size} color={color} />}} />
+            <Tabs.Screen name="Cart" component={CartNavigator} options={{headerShown: false, tabBarIcon: ({ color, size }) => <Ionicons name="cart" size={size} color={color} />}} />
+            <Tabs.Screen name="Info" component={InfoNavigator} options={{headerShown: false, tabBarIcon: ({ color, size }) => <Ionicons name="information-circle" size={size} color={color} />}} />
         </Tabs.Navigator>
     )
 }
