84092434afe389353fa838e1f3d238f6431ebeb7
antonDinkov <166150125+antonDinkov@users.noreply.github.com>
2026-01-25 12:25:10 +0700

    home screen header ready

--- src/rootScreens/homeTab/screens/HomeScreen.jsx
+++ src/rootScreens/homeTab/screens/HomeScreen.jsx
@@ -1,9 +1,48 @@
-import { Text, View } from "react-native";
+import { ScrollView, StyleSheet, Text, View } from "react-native";
 
 export default function HomeScreen(props) {
     return (
-        <View>
-            <Text>This is the home screen</Text>
-        </View>
+        <ScrollView>
+            <View style={Styles.header}>
+                <Text style={[Styles.headerText, {fontWeight: 'bold', fontSize: 28}]}>Tasty Bites</Text>
+                <View style={Styles.headerView}>
+                    <Text style={[Styles.headerText]}>⭐ 4.8 Rating</Text>
+                    <Text style={[Styles.headerText]}>•</Text>
+                    <Text style={[Styles.headerText]}>🕐 25-35 min</Text>
+                </View>
+                <Text style={[Styles.headerText]}>Fresh & Delicious Food Delivery Fast</Text>
+            </View>
+            <View>
+                <Text>Featurd Items Main Section Fragment</Text>
+                <ScrollView>
+                    <Text>Item Card Placeholder</Text>
+                </ScrollView>
+            </View>
+            <View>
+                <Text>Categories Main Section Fragment</Text>
+                <View>
+                    <Text>Category card Placehoder</Text>
+                </View>
+            </View>
+        </ScrollView>
+
     )
 }
+
+const Styles = StyleSheet.create({
+    header: {
+        backgroundColor: '#437afb',
+        borderBottomEndRadius: 20,
+        borderBottomStartRadius: 20,
+        padding: 20,
+        gap: 10
+    },
+    headerView: {
+        flexDirection: 'row',
+        gap: 10,
+    },
+    headerText: {
+        color: 'white',
+    }
+
+})
