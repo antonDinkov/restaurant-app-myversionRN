f12d0516410ac0dba077b3d3120f04c51eb464d1
antonDinkov <166150125+antonDinkov@users.noreply.github.com>
2026-02-01 14:26:39 +0700

    context provided to the root navigator

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import RootNavigator from './navigation/RootNavigator';
+import CartPtovider from './context/cart/CartProvider';
 
 export default function App() {
     return (
         <>
             <StatusBar style="auto" />
             <NavigationContainer>
-                <RootNavigator />
+                <CartPtovider>
+                    <RootNavigator />
+                </CartPtovider>
             </NavigationContainer>
         </>
 
