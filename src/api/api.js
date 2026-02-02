f73bb0d0461483fe0ab576b9a89c640f0ccdb24a
antonDinkov <166150125+antonDinkov@users.noreply.github.com>
2026-02-02 10:26:00 +0700

    port changed to 3000 for the server

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'http://192.168.1.151:3002',
+    baseURL: 'http://192.168.1.151:3000',
     headers: {"Content-Type": "application/json",}
 });
 
